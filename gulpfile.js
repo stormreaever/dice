a4e81185286f92f3b7a8bef8b03be473214f6e0d
kh <kathryn.hartog@gmail.com>
2018-05-23 23:52:09 -0400

    rejigger gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 
 
@@ -16,8 +17,9 @@ gulp.task('sass', function() {
     .pipe(sourcemaps.init())
       .pipe(sass({outputStyle: 'compressed'})
         .on('error', sass.logError))
-        .pipe(autoprefixer())
-      .pipe(sourcemaps.write())
+      .pipe(autoprefixer())
+      .pipe(rename({extname: '.min.css'}))
+      .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('assets/css'))
 });
 
@@ -29,45 +31,31 @@ gulp.task('scripts', function() {
     .pipe(sourcemaps.init())
       .pipe(uglify())
       .pipe(concat('scripts.min.js'))
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('assets/js'));
 });
 
 
-// gulp.task('scripts', function () {
-//   // set up the browserify instance on a task basis
-//   var b = browserify({
-//     entries: './assets/js/scripts/app.js',
-//     debug: true
-//   });
-//
-//   return b.bundle()
-//     .pipe(source('./scripts.min.js'))
-//     .pipe(buffer())
-//     .pipe(sourcemaps.init({loadMaps: true}))
-//         // Add transformation tasks to the pipeline here.
-//         .pipe(uglify())
-//         .on('error', gulplog.error)
-//     .pipe(sourcemaps.write('./'))
-//     .pipe(gulp.dest('./assets/js/'));
-// });
-
 // vendor scripts
 var vendorPaths = [
-  'node_modules/angular/angular.js',
-  'node_modules/angular-animate/angular-animate.js',
+  'node_modules/angular/angular.min.js',
+  'node_modules/angular-animate/angular-animate.min.js',
+  'node_modules/mathjs/dist/math.min.js',
   'assets/js/vendor/diceroll.browser.js',
 ];
 gulp.task('vendor-scripts', function() {
   return gulp.src(vendorPaths)
-    .pipe(concat('vendor.min.js'))
+    .pipe(sourcemaps.init())
+      .pipe(uglify())
+      .pipe(concat('vendor.min.js'))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('assets/js'));
 });
 
 
 
-// Watch task
-gulp.task('default',['sass', 'scripts', 'watch']);
+// Default -> Build errythang
+gulp.task('default',['sass', 'scripts', 'vendor-scripts']);
 
 // Watch task
 gulp.task('watch',function() {
